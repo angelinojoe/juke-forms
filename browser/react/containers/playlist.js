b677193e4397224e009e90da0bd8042fdf1b1d0e
Sokmean Nou <sokmean.nou@gmail.com>
2017-02-09 13:08:53 -0500

    playlist adding new name works!

--- browser/react/containers/playlist.js
+++ browser/react/containers/playlist.js
@@ -1,26 +1,61 @@
 import React, { Component } from 'react';
 import NewPlaylist from '../components/NewPlaylist';
+import axios from 'axios';
 
 
 export default class Playlist extends Component {
   constructor(props){
     super(props);
     this.state = {
-      inputValue: ''
+      inputValue: '',
+      hasChanged: false
     }
-    this.formSubmit = this.formSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.postPlaylist = this.postPlaylist.bind(this);
   }
 
-  formSubmit(event){
+  handleSubmit(event){
+    this.postPlaylist();
+    this.setState({
+      inputValue: '',
+      hasChanged: false
+    })
+    event.preventDefault();
+  }
+
+  handleChange(event){
     const value = event.target.value;
     this.setState({
-      inputValue: value
+      inputValue: value,
+      hasChanged: true
     })
   }
 
+  postPlaylist(){
+    return axios.post('/api/playlists', { name: this.state.inputValue })
+    .then(res => res.data)
+    .then(result => {
+      console.log(result) // response json from the server!
+    });
+  }
+
   render(){
-    console.log('formval', this.state.inputValue);
-    return <NewPlaylist formSubmit={this.formSubmit}/>
+    const empty = this.state.hasChanged && this.state.inputValue.length === 0;
+    const tooLong = this.state.inputValue.length > 16;
+    const validation = empty || tooLong;
+    return (
+      <div>
+        { empty ? <h5>Playlist name cannot be empty!</h5> : null }
+        { tooLong ? <h5>Playlist name cannot be  longer than 16 characters!</h5> : null }
+      <NewPlaylist
+        handleSubmit={ this.handleSubmit }
+        handleChange={ this.handleChange }
+        startingValue={ this.state.inputValue }
+        isLengthValid={ validation }
+        />
+    </div>
+    )
   }
 
 }
